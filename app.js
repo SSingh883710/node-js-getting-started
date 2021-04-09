6293315532d29e014eccdac95b3426fba5e12eef
SSingh883710 <ssingh883710@gmail.com>
2021-04-09 16:18:03 +0530

    Get route added.

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const PORT = process.env.PORT || 5000; //
 const cool = require('cool-ascii-faces');
 
 express()
-    .get('/', (req, res) => res.send('Hello there! We are successfully up & running.'))
     // .use(express.static(path.join(__dirname, 'public')))
     // .set('views', path.join(__dirname, 'views'))
     // .set('view engine', 'ejs')
     // .get('/', (req, res) => res.render('pages/index'))
     // .get('/cool', (req, res) => res.send(cool()))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .get('/', (req, res) => res.send('Hello there! We are successfully up & running.'))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
